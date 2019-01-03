af03ca82f188a3a6f5b03b43748dec02a661407a
Your Name <you@example.com>
2019-01-03 04:26:33 +0000

    Add core.cljs

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
                  [org.clojure/clojurescript "1.10.439"]]
   :plugins [[lein-ancient "0.6.15"]]
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
-                                  [figwheel-sidecar "0.5.18"]]}})
+                                  [figwheel-sidecar "0.5.18"]]}}
+  :source-paths ["src/main"])
---
+++ src/main/spike_node/core.cljs
@@ -0,0 +1,1 @@
+(ns spike-node.core)
