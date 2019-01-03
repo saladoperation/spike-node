983ab54cd15974e3014e80d67304eb79d06ae62d
Your Name <you@example.com>
2019-01-03 10:38:43 +0000

    Bind "j"

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.439"]
                  [cljs-node-io "1.1.2"]
+                 [cljsjs/mousetrap "1.5.3-0"]
+                 [frp "0.1.3"]
                  [me.raynes/fs "1.4.6"]]
   :plugins [[lein-ancient "0.6.15"]]
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
--- src/renderer/spike_node/core.cljs
+++ src/renderer/spike_node/core.cljs
@@ -1,1 +1,11 @@
-(ns spike-node.core)
+(ns spike-node.core
+  (:require [cljsjs.mousetrap]
+            [frp.core :as frp]))
+
+(frp/defe up down)
+
+(js/Mousetrap.bind "j" #(down))
+
+(js/Mousetrap.bind "k" #(up))
+
+(frp/activate)
