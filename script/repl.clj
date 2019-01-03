0c5778de081d0178181b91adcfddd1f766549b48
Your Name <you@example.com>
2019-01-03 04:43:32 +0000

    Compile ClojureSript

--- script/repl.clj
+++ script/repl.clj
@@ -1,16 +1,28 @@
 (ns repl
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [figwheel-sidecar.repl-api :as repl-api]))
 
 (def argument
   (first *command-line-args*))
 
+(def id
+  "app")
+
 (def build
   {:id           "app"
    :source-paths [(str "src/" argument)]
-   :compiler     {:output-to            (str "public/js/" argument ".js")
+   :compiler     {:output-to            (str "dev-resources/public/js/"
+                                             argument
+                                             ".js")
                   :main                 "spike-node.core"
                   :asset-path           (str/join "/" ["/js" argument "out"])
                   :source-map-timestamp true
                   :preloads             ['devtools.preload]
                   :external-config      {:devtools/config {:features-to-install :all}}}
    :figwheel     true})
+
+(repl-api/start-figwheel!
+  {:build-ids  [id]
+   :all-builds [build]})
+
+(repl-api/cljs-repl)
