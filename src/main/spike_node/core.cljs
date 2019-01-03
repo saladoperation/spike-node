f0262ffa180cbce94c74e310916bec5fd27e238f
Your Name <you@example.com>
2019-01-03 09:18:46 +0000

    Manage the window state

--- src/main/spike_node/core.cljs
+++ src/main/spike_node/core.cljs
@@ -5,14 +5,19 @@
 (def electron
   (js/require "electron"))
 
+(def window-state-keeper
+  (js/require "electron-window-state"))
+
 (.on (.-app electron)
      "ready"
      (fn [_]
-       (doto
-         (electron.BrowserWindow. {})
-         (.loadURL (str/join "/" ["file:/"
-                                  ;TODO deal with advanced optimizations
-                                  (-> js/__dirname
-                                      fs/dirname
-                                      fs/dirname)
-                                  "public/index.html"])))))
+       (let [window-state (window-state-keeper. {})]
+         (doto
+          (electron.BrowserWindow. window-state)
+          (.loadURL (str/join "/" ["file:/"
+                                   ;TODO deal with advanced optimizations
+                                   (-> js/__dirname
+                                       fs/dirname
+                                       fs/dirname)
+                                   "public/index.html"]))
+          window-state.manage))))
