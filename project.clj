428af0b54acd781fce618fc902323f7863c5dd61
Your Name <you@example.com>
2019-01-03 03:53:37 +0000

    Add clojure

--- project.clj
+++ project.clj
@@ -1,2 +1,4 @@
 (defproject spike-node "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.439"]]
   :plugins [[lein-ancient "0.6.15"]])
