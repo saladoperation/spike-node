6d3c97cee4ceb48febce21eda091a0ed5587f973
Your Name <you@example.com>
2019-01-03 04:21:26 +0000

    Define build

--- script/repl.clj
+++ script/repl.clj
@@ -1,1 +1,16 @@
-(ns repl)
+(ns repl
+  (:require [clojure.string :as str]))
+
+(def argument
+  (first *command-line-args*))
+
+(def build
+  {:id           "app"
+   :source-paths ["src"]
+   :compiler     {:output-to            (str "public/js/" argument ".js")
+                  :main                 "spike-node.core"
+                  :asset-path           (str/join "/" ["/js" argument "out"])
+                  :source-map-timestamp true
+                  :preloads             ['devtools.preload]
+                  :external-config      {:devtools/config {:features-to-install :all}}}
+   :figwheel     true})
