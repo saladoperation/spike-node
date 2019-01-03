e78a7d6b8c33a4c14ca270c3164dcec7094c0037
Your Name <you@example.com>
2019-01-03 04:22:27 +0000

    Add devtools

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject spike-node "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.439"]]
-  :plugins [[lein-ancient "0.6.15"]])
+  :plugins [[lein-ancient "0.6.15"]]
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
+                                  [figwheel-sidecar "0.5.18"]]}})
