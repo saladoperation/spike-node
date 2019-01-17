32f8584d9e76c9426e2c7f472898309aa4732b46
Your Name <you@example.com>
2019-01-17 23:25:41 +0000

    Replace single quotes with double quotes

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import AceEditor from "react-ace";
 import ReactDOM from "react-dom";
 import "brace/keybinding/vim";
-import 'brace/mode/latex';
+import "brace/mode/latex";
 import "brace/theme/terminal";
 
 window.AceEditor = AceEditor;
