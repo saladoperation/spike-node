d090973d6d3c0cb457cdabcd976c4d42d042f2a7
Your Name <you@example.com>
2019-01-17 00:59:13 +0000

    Don't use active-y

--- src/renderer/spike_node/core.cljs
+++ src/renderer/spike_node/core.cljs
@@ -2,7 +2,6 @@
   (:require [aid.core :as aid]
             [cats.core :as m]
             [cljsjs.mousetrap]
-            [com.rpl.specter :as s]
             [frp.core :as frp]
             [nano-id.core :refer [nano-id]]
             [reagent.core :as r]))
@@ -17,36 +16,28 @@
        (m/<$> keyword)
        (frp/stepper new)))
 
-(def file-y
-  (->> file-behavior
-       (frp/snapshot (m/<> (aid/<$ (aid/if-then pos?
-                                                dec)
-                                   up)
-                           (aid/<$ inc down)))
-       (m/<$> (fn [[f k]]
-                (partial s/transform* k f)))
-       (frp/accum {new 0})))
-
-(def active-y
-  ((aid/lift-a aid/funcall)
-    file-behavior
-    (frp/stepper {new 0} file-y)))
+(def cursor-y
+  (->> (m/<> (aid/<$ (aid/if-then pos?
+                                  dec)
+                     up)
+             (aid/<$ inc down))
+       (frp/accum 0)))
 
 (def size
   32)
 
 (defn app-component
-  [active-y*]
+  [cursor-y*]
   [:svg {:style {:background-color "black"
                  :height           "100%"
                  :width            "100%"}}
-   [:rect {:y      (* active-y* size)
+   [:rect {:y      (* cursor-y* size)
            :width  size
            :height size
            :stroke "white"}]])
 
 (def app
-  (m/<$> app-component active-y))
+  (m/<$> app-component cursor-y))
 
 (frp/run (partial (aid/flip r/render) (js/document.getElementById "app")) app)
 
