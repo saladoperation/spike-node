e484c5a7fd38124b9a652226a0de9c119989b266
Your Name <you@example.com>
2019-01-22 12:52:50 +0000

    Add a todo

--- src/main/spike_node/core.cljs
+++ src/main/spike_node/core.cljs
@@ -15,6 +15,7 @@
        (let [window-state (window-state-keeper. {})]
          (doto
            (helpers/electron.BrowserWindow. window-state)
+           ;TODO use path.join
            (.loadURL (str/join "/" ["file:/"
                                     ;TODO deal with advanced optimizations
                                     (-> js/__dirname
