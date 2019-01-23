fe7c2fb846faf9097fddf3c980e65f29848635a5
Your Name <you@example.com>
2019-01-23 03:18:32 +0000

    Define config

--- src/builder/spike_node/core.cljs
+++ src/builder/spike_node/core.cljs
@@ -1,4 +1,11 @@
 (ns spike-node.core)
 
+(def output
+  (last js/process.argv))
+
 (def builder
   (js/require "electron-builder"))
+
+(def config
+  {:linux {:directories {:output output}
+           :target      ["deb"]}})
