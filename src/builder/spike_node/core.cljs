ec55def8b03f1c2e09a9add958762a3c48b4961a
Your Name <you@example.com>
2019-01-29 01:26:21 +0000

    Set target to dist

--- src/builder/spike_node/core.cljs
+++ src/builder/spike_node/core.cljs
@@ -1,11 +1,16 @@
 (ns spike-node.core
-  (:require [spike-node.helpers :as helpers]))
-
-(def output
-  (last js/process.argv))
+  (:require [cljs-node-io.fs :as fs]
+            [spike-node.helpers :as helpers]))
 
 (def builder
   (js/require "electron-builder"))
+
+(def output
+  (if (-> js/process.argv
+          last
+          fs/file?)
+    "dist"
+    (last js/process.argv)))
 
 (def target
   ["zip"])
